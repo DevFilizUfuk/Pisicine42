f1e7a86ec1952c0c1ff4690c9f64bebdccbf608d
Nino Ladet <nladet@c4r2p4.42nice.fr>
2023-06-14 17:18:41 +0200

    2emt

--- c02/ex03/ft_str_is_alpha.c
+++ c02/ex03/ft_str_is_alpha.c
@@ -1,5 +1,33 @@
-#include <string.h>
 #include <stdio.h>
 
+int is_alpha(char c)
+{
+    return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+}
 
 int ft_str_is_alpha(char *str)
+{
+    if (*str == '\0') {
+        return 1;
+    }
+
+    while (*str != '\0') {
+        if (!is_alpha(*str)) {
+            return 0; 
+        }
+        str++; 
+    }
+
+    return 1; 
+}
+
+int main(int ac, char **av)
+{
+    int result;
+    char *str = av[1];
+    printf("%s\n", av[0]);
+    result = ft_str_is_alpha(str);
+    printf("%d\n", result);
+
+    return 0;
+}
